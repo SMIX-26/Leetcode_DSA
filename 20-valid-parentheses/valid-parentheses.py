2dc37649109753f0ba074587a3ce97f42b225392
Samikshya Jena <139850310+SMIX-26@users.noreply.github.com>
2026-07-03 00:43:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.61%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        if n%2==1:
+            return False
+        st = []
+
+        for ch in list(s):
+            #if uh get opening bracket then put in stack
+            if ch == '(' or ch == '{' or ch == '[':
+                st.append(ch)
+            #if uh get closing bracket then make sure to have that type opening bracket
+            else:
+                if len(st) == 0:
+                    return False
+                top = st.pop()
+                if ch == ')' and top != '(':
+                    return False
+                elif ch == '}' and top !='{':
+                    return False
+                elif ch == ']' and top !='[':
+                    return False
+        return len(st) == 0
+          
+                
