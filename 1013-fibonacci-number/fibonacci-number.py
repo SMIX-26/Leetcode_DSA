6796e609399e31db7e0ca344861ec56ce421d47e
Samikshya Jena <139850310+SMIX-26@users.noreply.github.com>
2026-07-25 19:37:51 +0530

    Time: 38 ms (93.68%) | Memory: 19.1 MB (88.60%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,5 +1,15 @@
 class Solution:
+    def rec(self, n , dp):
+        if n == 0 or n == 1:
+            return n 
+
+        if dp[n] != -1:
+            return dp[n]
+
+        #recursive case
+        dp[n] = self.rec(n-1,dp)+ self.rec(n-2,dp)
+        return dp[n]
     def fib(self, n: int) -> int:
-        if n==0 or n==1:
-            return n
-        return self.fib(n-1) + self.fib(n-2)
+        #memorisation top down approach
+        dp = [-1]*(n+1)
+        return self.rec(n,dp)
