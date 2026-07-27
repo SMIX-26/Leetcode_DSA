448c02040c70bdd344acddee333d65481d87e503
Samikshya Jena <139850310+SMIX-26@users.noreply.github.com>
2026-07-27 22:49:28 +0530

    Time: 6 ms (56.69%) | Memory: 26.8 MB (16.79%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,9 +1,4 @@
 class Solution:
-    def reverse(self, arr, left, right):
-            while left < right:
-                arr[left], arr[right] = arr[right], arr[left]
-                left += 1
-                right -= 1
     def rotate(self, arr: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -11,9 +6,15 @@ class Solution:
         n = len(arr)
 
         k = k % n
-        self.reverse(arr, 0, n - 1)
-        self.reverse(arr, 0, k - 1)
-        self.reverse(arr, k, n - 1)
+
+        def reverse(left:int, right:int) -> None:
+            while left < right:
+                arr[left], arr[right] = arr[right], arr[left]
+                left += 1
+                right -= 1
+        reverse( 0, n - 1)
+        reverse( 0, k - 1)
+        reverse( k, n - 1)
         
 
         
